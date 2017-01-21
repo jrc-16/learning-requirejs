3bdc28e529abcd8ce861a0e3ab69409b20f4339c
jason carney <jrcarney.16@gmail.com>
2017-01-21 22:57:37 +0000

    Updated basic example with how to use our requirejs modules. Also added comments.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-/* My first require module */
+/**
+ * main.js: This demonstrates how to use modules (external js files)
+ * and use them within our main.js file
+ */
 
-// jquery: an array of modules to be loaded and provided 
-// as inputs to the anonaymous function($)
-require(['jquery', 'message'], function($, message) {
-	$('#firstname').html(message);
+// []: an array of modules to be loaded and provided 
+// as inputs to the anonaymous function()
+// function: values returned from our modules
+require(['jquery', 'message', 'alertone'], function($, message, alertone) {
+	$('#firstname').html(message); // Use $ (jquery) and access our returned message modules value
+
+	alertone.shout(); // Use our alertone modules shout() method
+
+	alertone.testMe(); // Use our alertone modules testMe() method
+
+	$("body").prepend("<div>"+alertone.myAlertOneScopedVar+"</div>"); // Use our alertone modules Contructor objects property
 }); 
